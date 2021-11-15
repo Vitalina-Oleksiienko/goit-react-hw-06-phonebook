19107d1572857e2d918b8f825c74062501287b4e
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2021-11-15 16:00:13 +0200

    Update ContactList.js

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,11 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import style from "./contacts.module.css";
+import { connect } from 'react-redux';
+import { contactDelete } from '../redux/actions';
 
-export default function ContactList({ contacts, handleDelete }) {
-    return (
+
+function ContactList({ contacts, filter, handleDelete }) {
+
+  const filterContacts = () => { 
+    return contacts.filter((el) => {
+      const array = el.name.toLowerCase().split(" ");
+
+      for (let i = 0; i < array.length; i++) {
+        if (array[i].toLowerCase().match(new RegExp(`^${filter}`)) !== null) {
+          return true;
+        }
+      }
+      return false;
+    });
+  };
+
+
+  return (
       <ul className={style.list}>
-        {contacts.map((el) => (
+        {filterContacts().map((el) => (
           <li className={style.listItem} key={el.id}>
             {el.name}: {el.number}
             <button
@@ -20,7 +38,20 @@ export default function ContactList({ contacts, handleDelete }) {
         ))}
       </ul>
     );
-  }
+}
+
+const stateProps = state => {
+    return {
+        contacts: state?.contacts ?? [],
+        filter:state?.filter??""
+    }
+};
+
+const dispatchProps = dispatch => ({
+  handleDelete: e => dispatch(contactDelete(e.target.id))
+});
+
+export default connect(stateProps,dispatchProps)(ContactList);
 
 ContactList.propTypes = {
   contacts: PropTypes.array.isRequired,
