19cd85fb0d6529583b0aa81bb49f7c350d534a42
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2021-11-15 15:57:36 +0200

    add redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,25 @@
-import { createStore, combineReducers } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import contactReducer from "./reducer";
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import { reducer } from './reducer';
 
-const rootReducer = combineReducers({
-  contacts: contactReducer,
+const initData = () => {
+    const contact = localStorage.getItem("contacts");
+    if (contact) {
+      return JSON.parse(contact);
+    }
+    else { return [];}
+  };
+
+const preloadState = {
+  contacts: initData(),
+  filter: '',
+}
+
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  preloadedState: preloadState,
+  devTools: process.env.NODE_ENV === 'development',
 });
-
-const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
